a45c931d1376d1a7be7a7866cf24e201afd5370e
Manal Shaikh <shaikhmanal015@gmail.com>
2020-10-01 10:48:18 +0530

    Added error codes, useragent and userinput

--- check.py
+++ check.py
@@ -1,7 +1,22 @@
 import urllib3
 
 http = urllib3.PoolManager()
-
-r = http.request("GET", "https://shadowhosting.net")
+url = input("Website URL: ")
+r = http.request("GET", url, headers={
+    'User-Agent': 'Mozilla/5.0'
+})
+nf = 404
+rl = 429
+ok = 200
+br = 400
+fr = 403
+rt = 408
 rp = r.status
-print(rp)
+if rp == nf:
+    print("Not found")
+if rp == rl:
+    print("Rate limiting")
+if rp == ok:
+    print("Online")
+if rp == rt:
+    print("Request Timeout/Offline")
