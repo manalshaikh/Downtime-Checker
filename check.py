b951bf69c3d5379e4d9f3c07d06c5e1e1de40315
Manal Shaikh <shaikhmanal015@gmail.com>
2020-11-07 12:57:05 +0530

    Basic error handling for invalid URLs

--- check.py
+++ check.py
@@ -1,22 +1,29 @@
 import urllib3
+import requests
 
 http = urllib3.PoolManager()
 url = input("Website URL: ")
-r = http.request("GET", url, headers={
-    'User-Agent': 'Mozilla/5.0'
-})
-nf = 404
-rl = 429
-ok = 200
-br = 400
-fr = 403
-rt = 408
-rp = r.status
-if rp == nf:
-    print("Not found")
-if rp == rl:
-    print("Rate limiting")
-if rp == ok:
-    print("Online")
-if rp == rt:
-    print("Request Timeout/Offline")
+try:
+    response = requests.get(url)
+    r = http.request("GET", url, headers={
+        'User-Agent': 'Mozilla/5.0'
+    })
+    nf = 404
+    rl = 429
+    ok = 200
+    br = 400
+    fr = 403
+    rt = 408
+    rp = r.status
+    if rp == nf:
+        print("Not found")
+    if rp == rl:
+        print("Rate limiting")
+    if rp == ok:
+        print("Online")
+    if rp == rt:
+        print("Request Timeout/Offline")
+except requests.exceptions.MissingSchema as exception:
+    print("URL does not exist on the Internet. Try adding HTTP:// before the url. Eg. https://google.com")
+except requests.exceptions.ConnectionError as urlexception:
+    print("The connection to the URL has been terminated due to some error.")
